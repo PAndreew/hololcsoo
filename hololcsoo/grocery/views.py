6712f8fef992d82dc927e2c94b7f2d6ceea79e6a
Drew <thepandras@gmail.com>
2022-11-08 14:44:24 +0100

    Results ordered ascending, changed get_queryset

--- hololcsoo/grocery/views.py
+++ hololcsoo/grocery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 
-from .models import Grocery, Category, Food
+from .models import Grocery, Category, Food, Price
 
 
 class HomePageView(TemplateView):
@@ -10,13 +10,14 @@ class HomePageView(TemplateView):
 
 
 class SearchResultsView(ListView):
-    model = Food
+    model = Price
     template_name = 'home.html'
+    ordering = ['-value']
 
     def get_queryset(self):
         query = self.request.GET.get("q")
-        object_list = Food.objects.filter(
-            Q(name__icontains=query)
-        )
-        return object_list
-
+        day_of_newest_data = Price.objects.latest('timestamp').timestamp.day
+        price_list = Price.objects.filter(
+            Q(food__name__icontains=query) | Q(timestamp__day=day_of_newest_data)
+        ).order_by('value')
+        return price_list
